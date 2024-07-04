5cea9d73f07fd7250c2c49e5e333ef8313b419db
Dineshchand Geetha Ravichandran <>
2024-07-04 17:53:50 +0800

    modified

--- Programs/basic_program/1stProgram.py
+++ Programs/basic_program/1stProgram.py
@@ -2,11 +2,11 @@ print("hello world")
 
 print("enter 1st value")
 
-n1 = input()
+n1 = int(input())
 
 print("enter 2nd value")
 
-n2 = input()
+n2 = int(input())
 
 sum = n1 + n2
 print("sum is ", sum)
