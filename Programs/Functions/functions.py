795b857e57cd1382de0019cb4dbf4e457a16ae2d
Dineshchand Geetha Ravichandran <>
2024-07-30 19:12:01 +0800

    adding files

--- Programs/Functions/functions.py
+++ Programs/Functions/functions.py
@@ -1,18 +1,12 @@
 def my_function_1():
     print("Hello World")
 
-
-my_function_1()
-
-
 def my_function_2(fname):
     print(fname + " args")
 
-
-# abbitary args
+# arbitary args
 def my_function_3(*args):
     print((args))
-
 
 def addition(*args):
     sum = 0
@@ -20,25 +14,18 @@ def addition(*args):
         sum += ele
     print("unlimited sum ", sum)
 
-
-addition(10, 20, 30, 40, 50)
-
-
 def my_function_4(child3, child2, child1):
     print(child1, child2, child3)
-
 
 # keyword args
 def my_function_5(**fruit):
     print("fruits" + fruit["lname"])
     print("type", type(fruit))
 
-
 # default params
 def my_function_6(country="Norway"):
     print(country)
     return "hello"
-
 
 # pass collections
 # pass by reference
@@ -48,6 +35,8 @@ def my_function_7(fruits):
     fruits.append("Pomo")
 
 
+my_function_1()
+addition(10, 20, 30, 40, 50)
 my_function_2("hello")
 my_function_2("welcome")
 my_function_3("welcome", "hello", "world")
@@ -59,25 +48,6 @@ print(country)
 list1 = ["Apple", "Banana", "Mango"]
 my_function_7(list1.copy())
 print(list1)
-
-
-def recursive_fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return (recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2))
-
-
-n_terms = 5
-
-# check if the number of terms is valid
-if n_terms <= 0:
-    print("Invalid input ! Please input a positive value")
-else:
-    print("Fibonacci series:")
-for i in range(n_terms):
-    print("fib ", i, recursive_fibonacci(i))
-
 
 # lambda function - anonymous
 # lambda arguments : expression
