f77d1fbad92f0443f4dadcf066de371c53f5f59d
Dineshchand Geetha Ravichandran <>
2024-08-14 19:43:16 +0800

    adding files

---
+++ pydebugger_test.py
@@ -0,0 +1,13 @@
+import pdb
+
+def addition(a, b):
+    answer = a + b
+    return answer
+
+breakpoint()
+x = input("Enter first number : ")
+y = input("Enter second number : ")
+sum = addition(x, y)
+print(sum)
+
+#python pydebugger_test.py from console
